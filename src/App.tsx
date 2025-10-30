f987006ffeec65494465505d5677921a6787ce8d
Tim Martens <tbmartens@wisc.edu>
2025-10-29 20:45:27 -0500

    chore: update package version for eslint

--- src/App.tsx
+++ src/App.tsx
@@ -10,9 +10,7 @@ const App: React.FC = () => {
   return (
     <div className="container">
       <Header />
-
-      <h1>Welcome to the Twin Cities Cardboard Crew</h1>
-            {/* TODO: Added a welcome message/ about section  */}
+      {/* TODO: Added a welcome message/ about section  */}
       {/* TODO: add in the events list. Can be */}
       {/** TODO: a social section as well for email/discord */}
     </div>
--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 
+// may also want some sort of nav bar with skip links to the diff sections.
 const Header: React.FC = () => {
   return (
     <header className="u-posRelative u-sizeFull">
@@ -8,6 +9,7 @@ const Header: React.FC = () => {
         alt="TC3 Logo"
         height={150}
       />
+            <h1>Welcome to the Twin Cities Cardboard Crew</h1>
     </header>
   );
 };
