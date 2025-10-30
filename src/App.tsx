0393ec7075aa6d22daed06f393a80504d17194e5
Tim Martens <tbmartens@wisc.edu>
2025-10-29 20:35:31 -0500

    chore: Added some comments on next things to work on

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,20 @@
 import React from 'react';
-import Hello from './components/Hello';
 import Header from './components/Header/Header';
 
+/**
+ * DO we want to have separate pages for the about section and events?
+ * or just everything on the main page
+ * @returns 
+ */
 const App: React.FC = () => {
   return (
     <div className="container">
       <Header />
+
       <h1>Welcome to the Twin Cities Cardboard Crew</h1>
-      <Hello />
+            {/* TODO: Added a welcome message/ about section  */}
+      {/* TODO: add in the events list. Can be */}
+      {/** TODO: a social section as well for email/discord */}
     </div>
   );
 };
