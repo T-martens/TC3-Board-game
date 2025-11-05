7dfc0eed5c698afeda4a08194f684e1c6da2c48c
Tim Martens <tbmartens@wisc.edu>
2025-11-04 21:27:44 -0600

    feat: remove suitcss for now. use scss for everything

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,10 +1,10 @@
 import React from 'react';
-
+import './Header.scss';
 // may also want some sort of nav bar with skip links to the diff sections.
 const Header: React.FC = () => {
   return (
-    <header className="u-posRelative u-sizeFull">
-      <img src="/images/logos/logo-with-stem/TC3-Cherry-Black.png" alt="TC3 Logo" height={150} />
+    <header className="header">
+      <img src="/images/logos/logo-with-stem/TC3-Cherry-Black.png" alt="TC3 Logo" />
       <h1>Welcome to the Twin Cities Cardboard Crew</h1>
     </header>
   );
